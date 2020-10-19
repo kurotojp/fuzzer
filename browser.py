d13531a3b2a93c61a9f1d10f02f567a607615cfc
kurotojp <is0407ks@ed.ritsumei.ac.jp>
2020-10-19 16:19:18 +0900

    サーバが動いているかをtestするコードを追加

--- browser.py
+++ browser.py
@@ -6,7 +6,8 @@ import subprocess
 import sys
 import http.server
 import webbrowser
-import timeimport requests
+import time
+import pwn
 
 
 SERVER_PORT :int = 8000
@@ -21,20 +22,32 @@ chrome = webbrowser.get("google-chrome")
 def fuzzing(fuzz : str):
     chrome.open(target_url + "#" + fuzz)
 
-
 def test():
-    while():
-        print("server.pyがうまく起動していません")
-        time.sleep(5)
-    
-    while():
-        print("detect.pyがうまく起動していません")
-        time.sleep(5)
+    while(True):
+        try:
+            io = pwn.remote('127.0.0.1', SERVER_PORT)
+            io.send('GET /\r\n\r\n')
+            req = io.recv()
+            print("[*]server.pyの起動を確認！")
+            break
+        except:
+            print("[-]server.pyがうまく起動していません")
+            time.sleep(5)
 
-
+    while(True):
+        try:
+            io = pwn.remote('127.0.0.1', DETECT_PORT)
+            io.send('GET /\r\n\r\n')
+            req = io.recv()
+            print("[*]detect.pyの起動を確認！")
+            break
+        except:
+            print("[-]detect.pyがうまく起動していません")
+            time.sleep(5)
 
 
 if __name__ == '__main__':
+    test()
     fuzz :str = "location.href='" + fuzz_url + "';" 
     fuzzing(fuzz)
 
