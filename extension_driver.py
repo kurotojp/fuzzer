b968ed1e029704c73fc5d1c544244c986da4e672
kurotojp <is0407ks@ed.ritsumei.ac.jp>
2020-10-26 16:22:32 +0900

    test追加して、fuzzingのテストベクトルでとりあえず発火するようにした

--- extension_driver.py
+++ extension_driver.py
@@ -1,5 +1,6 @@
 import selenium.webdriver
 import pwn
+import time
 
 SERVER_PORT :int = 8000
 DETECT_PORT :int = 8001
@@ -7,16 +8,46 @@ default_url :str = "http://127.0.0.1"
 target_url :str = default_url + ":" + str(SERVER_PORT)
 fuzz_url :str = default_url + ":" + str(DETECT_PORT)
 
-
 options = selenium.webdriver.ChromeOptions()
 options.add_argument('--disable-gpu')
 #options.add_argument("--headless")
 options.add_extension("~/extension/xss2/dist/chrome.crx")
 driver = selenium.webdriver.Chrome("/usr/bin/chromedriver", options=options)
 
-driver.get(target_url)
-driver.add_cookie({"name":"key", "value":"value"})
 
-fuzz1 = "location.href="
-fuzz2 = ""
-driver.get(target_url + "#" + fuzz1 + fuzz_url + fuzz2)
+def test():
+    while(True):
+        try:
+            io = pwn.remote('127.0.0.1', SERVER_PORT)
+            io.send('GET /\r\n\r\n')
+            req = io.recv()
+            #print("[*]server.pyの起動を確認！")
+            break
+        except:
+            #print("[-]server.pyがうまく起動していません")
+            time.sleep(5)
+
+    while(True):
+        try:
+            io = pwn.remote('127.0.0.1', DETECT_PORT)
+            io.send('GET /\r\n\r\n')
+            req = io.recv()
+            #print("[*]detect.pyの起動を確認！")
+            break
+        except:
+            #print("[-]detect.pyがうまく起動していません")
+            time.sleep(5)
+
+def fuzzing():
+    #driver.get(target_url)
+    #driver.add_cookie({"name":"key", "value":"value"})
+    fuzz1 = "location.href='" + fuzz_url  + "';"
+    #fuzz1 = "alert(document.cookie);"
+    fuzz2 = ""
+    #driver.get(target_url + "#" + fuzz1 + fuzz_url + fuzz2)
+    driver.get(target_url + "#" + fuzz1)
+
+if __name__ == '__main__':
+    #test()
+    fuzzing()
+
