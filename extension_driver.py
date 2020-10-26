ad2b3fd507cb1d1022ced66ce04e536180af1c9b
kurotojp <is0407ks@ed.ritsumei.ac.jp>
2020-10-26 15:30:37 +0900

    微調整

--- extension_driver.py
+++ extension_driver.py
@@ -1,17 +1,22 @@
-import os
 import selenium.webdriver
+import pwn
+
+SERVER_PORT :int = 8000
+DETECT_PORT :int = 8001
+default_url :str = "http://127.0.0.1"
+target_url :str = default_url + ":" + str(SERVER_PORT)
+fuzz_url :str = default_url + ":" + str(DETECT_PORT)
+
 
 options = selenium.webdriver.ChromeOptions()
 options.add_argument('--disable-gpu')
 #options.add_argument("--headless")
-#options.add_extension("/home/cysec/extension/vulnerability-chrome-extension/dist/chrome.crx")
-options.add_extension("/home/cysec/extension/xss2/dist/chrome.crx")
+options.add_extension("~/extension/xss2/dist/chrome.crx")
 driver = selenium.webdriver.Chrome("/usr/bin/chromedriver", options=options)
 
-url = "http://sample.cysec:8000/"
-driver.get(url)
+driver.get(target_url)
 driver.add_cookie({"name":"key", "value":"value"})
 
-url = "http://sample.cysec:8000/#location.href='http://sample.cysec:8001'"
-driver.get(url)
+fuzz = "location.href="
 
+driver.get(target_url + "#" + fuzz + fuzz_url)
