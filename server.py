98af3869fd8eebc779f59fd35a57401dd644538a
kurotojp <is0407ks@ed.ritsumei.ac.jp>
2021-01-08 13:55:01 +0900

    少し変更したとこあるかも

--- server.py
+++ server.py
@@ -4,13 +4,15 @@ import os
 import json
 import flask
 import sys
+import argparse
 
 SERVER_PORT :int = 8000
 DETECT_PORT :int = 8001
 default_url :str = "http://127.0.0.1"
 https = False
-def http_or_https():
-    json_file = open(os.environ['HOME'] + '/extension/xss2/dist/chrome/manifest.json', 'r')
+def http_or_https(manifest):
+    json_file = open(manifest, 'r')
+    
     json_manifest = json.load(json_file)
     for element in json_manifest['permissions']:
         if "https" in element:
@@ -43,7 +45,25 @@ def end():
     return "End"
 
 if __name__ == '__main__':
-    http_or_https()
+    parser = argparse.ArgumentParser("python3 server.py extension")
+    parser.add_argument("extension", help="Where extension")
+
+    try:
+        args = parser.parse_args()
+    except:
+        print("[-] No extension selected")
+        exit(1)
+
+    if os.path.exists(args.extension) is False:
+        print("[-] No extension! Path miss?")
+        exit(1)
+
+    manifest = args.extension.split(".crx")[0] + "/manifest.json"
+    if os.path.exists(manifest) is False:
+        print("[-] manifestfile is not found!")
+        exit(1)
+
+    http_or_https(manifest)
     if https is False:
         app.run(debug=False, host="127.0.0.1", port=SERVER_PORT)
     else:
